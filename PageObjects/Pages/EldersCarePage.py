684590bf002c60b39ac9127b2c900ad490fac3da
Kreshnik <khyseni21@gmail.com>
2018-03-26 09:17:03 +0200

    ily

--- PageObjects/Pages/EldersCarePage.py
+++ PageObjects/Pages/EldersCarePage.py
@@ -1,6 +1,4 @@
 from PageObjectLibrary import PageObject
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 
 
 class EldersCarePage(PageObject):
@@ -17,9 +15,23 @@ class EldersCarePage(PageObject):
         return True
 
     _locators = {
-            "drg": "xpath=//div[@class='grip cbutton cbutton--effect-jagoda cbutton--click']",
-            "trg": "xpath=//div[@class='grip cbutton cbutton--effect-jagoda cbutton--click']"
+
+            "Weiter": "Weiter"
     }
+
+    def answer_questions(self, *options):
+        for a in options:
+            self.se2lib.wait_until_page_contains(a)
+            self.se2lib.click_element('xpath=//*[contains(text(),"' + a + '")]')
+            self.se2lib.wait_until_page_contains(self.locator.Weiter)
+            self.se2lib.click_link(self.locator.Weiter)
+
+    def check_results(self, *options):
+        self.se2lib.wait_until_page_contains('Ihre Filter')
+        for a in options:
+            self.se2lib.wait_until_page_contains(a)
+
+
 
 
 
